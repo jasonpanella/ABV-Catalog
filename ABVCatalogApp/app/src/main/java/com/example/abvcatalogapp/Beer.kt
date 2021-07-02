80c38db5c4c749c2ae2fa8784f090dc6f99c3134
JPanella <jpanella@acosta.com>
2021-07-02 11:40:11 -0400

    read in csv file

---
+++ ABVCatalogApp/app/src/main/java/com/example/abvcatalogapp/Beer.kt
@@ -0,0 +1,28 @@
+package com.example.abvcatalogapp
+
+data class Beer(
+    val breweryId: Int,
+    val name: String,
+    val cat: Int,
+    val style: Int,
+    val abv: Float,
+    val ibu: Float,
+    val srm: Float,
+    val upc: Int,
+    val descript: String
+){
+//    override fun toString(): String {
+//        return "Customer [id=" + id + ", name=" + name + ", address=" + address + ", age=" + age + "]"
+//    }
+}
+//
+//val name: String,
+//val cat: Int,
+//val style: Int,
+//val abv: Float,
+//val ibu: Float,
+//val srm: Float,
+//val upc: Int,
+//val filepath: String,
+//val descript: String,
+//val add_user: Int
--- ABVCatalogApp/app/src/main/java/com/example/abvcatalogapp/MainActivity.kt
+++ ABVCatalogApp/app/src/main/java/com/example/abvcatalogapp/MainActivity.kt
@@ -1,28 +1,29 @@
 package com.example.abvcatalogapp
 
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navView: NavigationView
+    lateinit var beer: Beer
+
+
+    var dbHandler = DBHelper(this, null)
+    var dataList = ArrayList<HashMap<String, String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +46,88 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_profile -> {
+                //init db
+
+                val BEER_BREWERY_IDX = 1
+                val BEER_NAME_IDX = 2
+                val BEER__CAT_IDX = 3
+                val BEER__STYLE_IDX = 4
+                val BEER__ABV_IDX = 5
+                val BEER__IBU_IDX = 6
+                val BEER__SRM_IDX = 7
+                val BEER__UPC_IDX = 8
+                val BEER__DESCRIPT_IDX = 9
+
+
+                //var fileReader: BufferedReader? = null
+                val beers = ArrayList<Beer>()
+                val coll = ArrayList<String>()
+                var line: String?
+
+                val inputStream: InputStream = resources.openRawResource(R.raw.beers)
+                val fileReader = BufferedReader(InputStreamReader(inputStream, Charset.forName("UTF-8")))
+
+                // Read CSV header
+                fileReader.readLine()
+
+                // Read the file line by line starting from the second line
+                line = fileReader.readLine()
+                while (line != null) {
+
+                    val splitMe = line
+                    val splitByQuote =
+                        splitMe.split("\"".toRegex()).toTypedArray()
+                    val splitByComma: Array<Array<String?>?> =
+                        arrayOfNulls(splitByQuote.size)
+                    for (i in splitByQuote.indices) {
+                        val part = splitByQuote[i]
+                        if (i % 2 == 0) {
+                            splitByComma[i] = part.split(",".toRegex()).toTypedArray()
+                        } else {
+                            splitByComma[i] = arrayOfNulls(1)
+                            splitByComma[i]?.set(0, part)
+                        }
+                    }
+
+
+                    for (parts in splitByComma) {
+                        if (parts != null) {
+                            for (part in parts) {
+                                if (part != null) {
+                                    if (part.length > 0) {
+                                        coll.add(part)
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+
+                    //val tokens = line.split(",(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*\$)")
+                    if (coll.size > 0) {
+                        val beer = Beer(
+                            Integer.parseInt(coll[BEER_BREWERY_IDX].replace("\"", "")),
+                            coll[BEER_NAME_IDX].replace("\"", ""),
+                            Integer.parseInt(coll[BEER__CAT_IDX].replace("\"", "")),
+                            Integer.parseInt(coll[BEER__STYLE_IDX].replace("\"", "")),
+                            coll[BEER__ABV_IDX].replace("\"", "").toFloat(),
+                            coll[BEER__IBU_IDX].replace("\"", "").toFloat(),
+                            coll[BEER__SRM_IDX].replace("\"", "").toFloat(),
+                            Integer.parseInt(coll[BEER__UPC_IDX].replace("\"", "")),
+                            coll[BEER__DESCRIPT_IDX].replace("\"", ""))
+
+                        beers.add(beer)
+                    }
+
+                    line = fileReader.readLine()
+                }
+
+                // Insert the new beer list
+                for (customer in beers) {
+                    dbHandler.insertRow(customer)
+                }
+
+                // Read the data
                 Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
             }
             R.id.nav_messages -> {
